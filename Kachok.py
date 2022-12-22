87d9a11227d0e2de7410693b03f752ac6d1264e9
HappyAnd_13 <117274638+HappyAnd13@users.noreply.github.com>
2022-12-22 13:32:02 +0000

    Add files via upload

---
+++ Kachok.py
@@ -0,0 +1,40 @@
+from pytube import YouTube
+from pytube.cli import on_progress
+import pytube.request
+import random
+import time
+
+pytube.request.default_range_size=500000
+
+def typewrite(num1,num2,text):
+    for c in text:
+        r=random.uniform(num1,num2)
+        time.sleep(r)
+        print(c,end='',flush=True)
+
+def completed(a,b):
+    completed='\nDownload Complete\n'
+    size='File Size'+str(stream.filesize/1000000) +' Mb\n'
+    title='Title:' +stream.title+'\n'
+    desc='Description:'+yt.description+'\n'
+    author='Author:'+yt.author+'\n'
+    length='Video Length'+str(yt.length)+' Seconds\n'
+
+    txt_list=[completed,size,title,desc,author,length]
+
+    for t in txt_list:
+        typewrite(.05,.1,t)
+        print('-'*60)
+
+typewrite(.05,.1,"KACHOK version 0.1 (c) 2022, HappyAnd_13")
+url=input('\nPlease Enter The URL: ')
+
+try:
+    yt=YouTube(url, on_progress_callback=on_progress, on_complete_callback=completed)
+    stream=yt.streams.filter(progressive=True,file_extension='mp4').get_highest_resolution()
+    typewrite(.05,.1,'Download is starting...\n')
+    stream.download()
+except:
+    print('Something went wrong!')
+
+
